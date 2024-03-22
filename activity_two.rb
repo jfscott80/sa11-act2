da7003333dfa3ea009cb9feab3f316d758aac2f7
John Scott <jfscott@memphis.edu>
2024-03-22 16:49:49 -0500

    hashtags, password complete

---
+++ activity_two.rb
@@ -0,0 +1,53 @@
+# activity 1
+def extract_hashtags(text)
+    pattern = /(?:#(?!(?:\s|$)))(\w+)(?=\s|$)/i
+    hashtags = text.scan(pattern)
+    hashtags.flatten
+  end
+  
+puts extract_hashtags("Love this #beautiful day! #sunny")
+
+ 
+# activity 2
+# must contain at least one uppercase, one lowercase
+# one digit, and be 8 to 16 characters long
+def valid_password?(password)
+  pattern = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z\d]{8,16}/
+  password.match?(pattern)
+end
+  
+puts valid_password?("Passw0rd")
+puts valid_password?("pass")
+
+# activity 3
+# find all numeric values in a string
+def find_numbers(text)
+  pattern = nil
+  text.scan(pattern)
+end
+
+puts find_numbers("The order was placed for 100.25 units at a price of 20.75 each.")
+
+
+# activity 4
+# match dates in the format YYYY-MM-DD.
+def valid_date?(date)
+  pattern = nil
+  date.match?(pattern)
+end
+
+puts valid_date?("2023-03-15")
+puts valid_date?("15-03-2023")
+
+# activity 5
+# match log entries beginning with a severity level
+# (INFO, WARN, or ERROR), followed by a message
+def parse_log_entry(log)
+  pattern = nil
+  match = log.match(pattern)
+  [match[1], match[2]] if match
+end
+
+log_entry = "ERROR: Disk space low"
+puts parse_log_entry(log_entry).inspect
+
